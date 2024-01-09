cfbacccd20f873c20751503fb014b57ceb7ef1ee
BIBruington <psychological_chemist@hotmail.com>
2024-01-08 21:22:19 -0600

    Suspense loading

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,13 +1,21 @@
+import { Suspense } from "react";
 import TicketList from "./TicketList";
+import Loading from "../loading";
 
 export default function Tickets() {
   return (
     <main>
       <nav>
         <h2>Tickets</h2>
-        <p><small>Currently open tickets.</small></p>
+        <p>
+          <small>Currently open tickets.</small>
+        </p>
       </nav>
-      <TicketList />
+      
+    {/* Suspence defines where i want the loading to happen */}
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   );
 }
